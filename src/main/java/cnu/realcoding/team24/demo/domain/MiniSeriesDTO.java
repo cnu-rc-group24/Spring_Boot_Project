114f3a05f2db39d4d967838f9f6da1ead731fd9d
Kim HyunSeop <hyunseop95@gmail.com>
2019-05-05 19:32:46 +0900

    Add LeaguePosition DTO file
    Summoner DTO에서 받아온 정보를 이용하여 Position DTO를 가지고 온다. 
    
    *https://developer.riotgames.com/api-methods/#league-v4/GET_getAllLeaguePositionsForSummoner

---
+++ src/main/java/cnu/realcoding/team24/demo/domain/MiniSeriesDTO.java
@@ -0,0 +1,13 @@
+package cnu.realcoding.team24.demo.domain;
+
+public class MiniSeriesDTO {
+    /*progress	string
+    losses	int
+    target	int
+    wins	int*/
+
+    private String progress;
+    private int losses;
+    private int target;
+    private int wins;
+}
